06a1e15e3b0d30482800f4af49206898b14474a4
VitaliySlivchuk <41226137+VitaliySlivchuk@users.noreply.github.com>
2023-03-09 13:31:45 +0200

    1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,33 @@
+import { Routes, Route, NavLink } from 'react-router-dom';
+import styled from 'styled-components';
+import Home from './Home';
+import About from './About';
+import Products from './Products';
+
+const StyledLink = styled(NavLink)`
+  color: black;
+
+  &.active {
+    color: orange;
+  }
+`;
+
 export const App = () => {
-  return <div>1</div>;
+  return (
+    <div>
+      <nav>
+        <StyledLink to="/" end>
+          Home
+        </StyledLink>
+        <StyledLink to="/about">About</StyledLink>
+        <StyledLink to="/products">Products</StyledLink>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/products" element={<Products />} />
+      </Routes>
+    </div>
+  );
 };
